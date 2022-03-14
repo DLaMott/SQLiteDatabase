0ec32780ebc0ac98d064b05fc35efba939d2d6b9
GothamsJoker <dylanlamott@gmail.com>
2022-03-14 08:21:05 -0400

    Removed unneeded error throw

--- src/main/java/com/Dylan/SQLiteTest.java
+++ src/main/java/com/Dylan/SQLiteTest.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class SQLiteTest {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
 
         try {
 
